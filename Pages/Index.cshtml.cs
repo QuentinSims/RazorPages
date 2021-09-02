80e6789d6d6289fb861927e7e3ec6608a008f783
Quentin <quentinsims89@gmail.com>
2021-09-02 15:10:36 +0200

    Add project files.

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+
+namespace TestApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string FullName { get; set; }
+        public string LinkedInUserName { get; set; }
+        public int experience { get; set; }
+        public List<string> skills { get; set; }
+
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            FullName = "Quentin Sims";
+            LinkedInUserName = "Quentin Sims";
+            experience = 4;
+            skills = new List<string>
+            {
+                "C#",
+                "Python",
+                "Javascript",
+                "SQL",
+                ".Net"
+            };
+        }
+    }
+}
---
+++ Pages/Projects.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TestApp
+{
+    public class ProjectsModel : PageModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Technologies { get; set; }
+
+        public string GithubUsername { get; set; }
+
+        public void OnGet()
+        {
+            GithubUsername = "QuentinSims";
+            Title = "1";
+            Description = "Html";
+            Technologies = "Html";
+        }
+
+    }
+}
